1a0e55a89a648b2c0a30623db7028fd4438dcb25
DivyaPrabha19 <147841695+DivyaPrabha19@users.noreply.github.com>
2023-11-18 18:00:13 +0530

    Add files via upload

---
+++ nested for loop.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+      int a[2][3]={6,3,4,5,9,8};
+      for(int i=0;i<=1;i++)
+      {
+          for(int j=0;j<=2;j++)
+          {
+             printf("%d \t",a[i][j]);
+          }
+          printf("\n");
+         
+      }
+     
+   }
+     
