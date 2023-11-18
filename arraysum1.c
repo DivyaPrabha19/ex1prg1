0c6eda3a014f2757cfa4fe3566ca13eb6d3c007a
DivyaPrabha19 <147841695+DivyaPrabha19@users.noreply.github.com>
2023-11-18 12:33:30 +0530

    Add files via upload

---
+++ arraysum1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+    int lang[10],sum;
+    lang[0]=2;
+    lang[1]=1;
+    lang[2]=2;
+    lang[3]=1;
+    sum=lang[0]+lang[1]+lang[2]+lang[3];
+    printf("sum is %d",sum);
+    
+}
