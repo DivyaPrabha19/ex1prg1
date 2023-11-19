5b24505a440dcb3a33e5e493eead548abf026ed1
DivyaPrabha19 <147841695+DivyaPrabha19@users.noreply.github.com>
2023-11-19 09:12:04 +0530

    Add files via upload

---
+++ escape sequences.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+
+   printf("\n it is used to print the statements in new line");
+   printf("\a it is used to give bell sound ");
+   printf("\\ it is used to print the statement with backslash");
+   printf("\t it is used to give tab spaces ");
+}
+
