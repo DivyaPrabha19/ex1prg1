35c1256b43251b93d38961c0be8e2c9b872f7460
DivyaPrabha19 <147841695+DivyaPrabha19@users.noreply.github.com>
2023-11-19 08:54:01 +0530

    Add files via upload

---
+++ perfect number.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+   int a,num1,num2,num3,sum;
+   printf("Enter three digit number:")
+   scanf("%d",&a);
+   num1=pow((a/100),3);
+   num2=pow(((a/10)%10),3);
+   num3=pow((a%10),3);
+   sum=num1+num2+num3;
+   printf("The Perfect Number is %d",sum);
+   
+   
+}
+
